c905e6dedf84cf6f434d855f8cdd26542f64d3fc
Udunen <cole.huffine@gmail.com>
2024-11-10 23:44:46 -0600

    added toggle to the theremin, and fixed incorrect note frequencies
    toggle to start adding premade songs

--- include/constants.h
+++ include/constants.h
@@ -8,6 +8,10 @@ namespace constants {
     const int ECHO = 6;
     const int BUZZER = 8;
 
+    const int SONG_1 = 10;
+    const int SONG_2 = 11;
+    const int TOGGLE = 12;
+
     const float SPEED_OF_SOUND = 0.01350393302; // speed of sound in inches/microsecond
 
     const int LOW_DIST = 1; // minimum distance we can be from the sensor
@@ -16,28 +20,42 @@ namespace constants {
 
     // these are all of the notes we use in hertz
     namespace notes {
-        const float C_4 = 525.25;
-        const float C_SHARP_4 = 554.37;
-        const float D_4 = 587.33;
-        const float D_SHARP_4 = 622.25;
-        const float E_4 = 659.26;
-        const float F_4 = 698.46;
-        const float F_SHARP_4 = 739.99;
-        const float G_4 = 783.99;
-        const float G_SHARP_4 = 830.61;
-        const float A_4 = 880;
-        const float A_SHARP_4 = 932.33;
-        const float B_4 = 987.77;
+        const float C_3 = 130.81;
+        const float C_SHARP_3 = 138.59;
+        const float D_3 = 146.83;
+        const float D_SHARP_3 = 155.56;
+        const float E_3 = 164.81;
+        const float F_3 = 174.61;
+        const float F_SHARP_3 = 185;
+        const float G_3 = 196;
+        const float G_SHARP_3 = 207.65;
+        const float A_3 = 220;
+        const float A_SHARP_3 = 233.08;
+        const float B_3 = 246.94;
+        
+        // probably only gonna use these for theremin
+        const float C_4 = 261.63;
+        const float C_SHARP_4 = 277.18;
+        const float D_4 = 293.66;
+        const float D_SHARP_4 = 311.13;
+        const float E_4 = 329.63;
+        const float F_4 = 349.23;
+        const float F_SHARP_4 = 369.99;
+        const float G_4 = 392;
+        const float G_SHARP_4 = 415.30;
+        const float A_4 = 440;
+        const float A_SHARP_4 = 466.16;
+        const float B_4 = 493.88;
 
-        const float C_5 = 1046.50;
-        const float C_SHARP_5 = 1108.73;
-        const float D_5 = 1174.66;
-        const float D_SHARP_5 = 1244.51;
-        const float E_5 = 1318.51;
-        const float F_5 = 1396.91;
-        const float F_SHARP_5 = 1476.98;
-        const float G_5 = 1567.98;
-        const float G_SHARP_5 = 1661.22;
+        const float C_5 = 523.25;
+        const float C_SHARP_5 = 554.37;
+        const float D_5 = 587.33;
+        const float D_SHARP_5 = 622.25;
+        const float E_5 = 659.26;
+        const float F_5 = 698.46;
+        const float F_SHARP_5 = 739.99;
+        const float G_5 = 783.99;
+        const float G_SHARP_5 = 830.61;
         const float A_5 = 880;
         const float A_SHARP_5 = 932.33;
         const float B_5 = 987.77;
--- src/main.cpp
+++ src/main.cpp
@@ -7,41 +7,63 @@ using namespace constants;
 float duration, distance; // variables for tracking distance from the sensor
 float inchesToPitch(float inches); // initialize function so we can convert inches to pitch
 
+int togglePushCounter = 0;
+int toggleState = 0;
+int lasttoggleState = 0;
+
 void setup() {
   // initialize pins
   pinMode(TRIG, OUTPUT);
   pinMode(ECHO, INPUT);
   pinMode(BUZZER, OUTPUT);
+  pinMode(TOGGLE, INPUT);
+  pinMode(SONG_1, INPUT);
+  pinMode(SONG_2, INPUT);
   Serial.begin(9600); // begin serial so we can track data
 }
 
 void loop() {
-  // set TRIG pin to low to ensure a clean pulse of sound from the distance sensor
-  digitalWrite(TRIG, LOW);
-  delay(2);
+  toggleState = digitalRead(TOGGLE);
 
-  // output sound for the sensor
-  digitalWrite(TRIG, HIGH);
-  delay(10); // make sound 10 milliseconds long to ensure the ECHO pin gets it
-  digitalWrite(TRIG, LOW);
-
-  duration = pulseIn(ECHO, HIGH); // track duration it took for the sound to reach the ECHO input. In microseconds.
-
-  // convert microseconds to inches
-  // SPEED_OF_SOUND is in inches/microseconds
-  // divide by 2 because the sound has to leave the sensor, bounce off, and come back, resulting in double the distance
-  distance = (duration * SPEED_OF_SOUND) / 2; 
-
-  // if the distance is in our range, output tone from the buzzer
-  if(distance >= LOW_DIST && distance <= HIGH_DIST) {
-    Serial.print("Distance: ");
-    Serial.println(distance);
-    Serial.print("Current Frequency: ");
-    Serial.println(autoTune(inchesToPitch(distance)));
-
-    // output autoTuned pitch from the buzzer. make the duration 100 to ensure constant noise from the buzzer.
-    tone(BUZZER, autoTune(inchesToPitch(distance)), 100);
+  if (toggleState != lasttoggleState) {
+    if (toggleState == HIGH) {
+      togglePushCounter++;
+    }
+    delay(50);
   }
+  lasttoggleState = toggleState;
+
+  if (togglePushCounter % 2 == 0) {
+    // set TRIG pin to low to ensure a clean pulse of sound from the distance sensor
+    digitalWrite(TRIG, LOW);
+    delay(2);
+
+    // output sound for the sensor
+    digitalWrite(TRIG, HIGH);
+    delay(10); // make sound 10 milliseconds long to ensure the ECHO pin gets it
+    digitalWrite(TRIG, LOW);
+
+    duration = pulseIn(ECHO, HIGH); // track duration it took for the sound to reach the ECHO input. In microseconds.
+
+    // convert microseconds to inches
+    // SPEED_OF_SOUND is in inches/microseconds
+    // divide by 2 because the sound has to leave the sensor, bounce off, and come back, resulting in double the distance
+    distance = (duration * SPEED_OF_SOUND) / 2; 
+
+    // if the distance is in our range, output tone from the buzzer
+    if(distance >= LOW_DIST && distance <= HIGH_DIST) {
+      Serial.print("Distance: ");
+      Serial.println(distance);
+      Serial.print("Current Frequency: ");
+      Serial.println(autoTune(inchesToPitch(distance)));
+
+      // output autoTuned pitch from the buzzer. make the duration 100 to ensure constant noise from the buzzer.
+      tone(BUZZER, autoTune(inchesToPitch(distance)), 100);
+    }
+  } else {
+    
+  }
+
 }
 
 // convert inches to pitch using an equation
@@ -62,4 +84,44 @@ float inchesToPitch(float inches) {
             change in inches
   */
   return ((maxPitch-minPitch)/(maxInches-minInches))*(inches - LOW_DIST) + minPitch;
+}
+
+void hotToGo() {
+  tone(BUZZER, A_SHARP_3, 1000); // h
+  delay(1000);
+  tone(BUZZER, C_SHARP_4, 1000); // o
+  delay(1000);
+  tone(BUZZER, C_SHARP_4, 1000); // t
+  delay(1000);
+  tone(BUZZER, C_SHARP_4, 1000); // t
+  delay(1000);
+  tone(BUZZER, D_SHARP_4, 1000); // o
+  delay(1000);
+  tone(BUZZER, C_SHARP_4, 1000); // g
+  delay(1000);
+  tone(BUZZER, D_SHARP_4, 1000); // o 
+  delay(1000);
+  tone(BUZZER, C_SHARP_4, 1000); // o PART 2
+  delay(1000);
+
+  tone(BUZZER, A_SHARP_3, 1000); // you
+  delay(1000);
+  tone(BUZZER, C_SHARP_4, 1000); // can
+  delay(1000);
+  tone(BUZZER, F_SHARP_4, 1000); // take
+  delay(1000);
+  tone(BUZZER, G_SHARP_4, 1000); // me
+  delay(1000);
+  tone(BUZZER, G_SHARP_4, 1000); // hot
+  delay(1000);
+  tone(BUZZER, A_SHARP_4, 1000); // to
+  delay(1000);
+  tone(BUZZER, G_SHARP_4, 1000); // go
+  delay(1000);
+  tone(BUZZER, F_SHARP_4, 1000); // go PART 2
+  delay(1000);
+}
+
+void minecraft() {
+  
 }
